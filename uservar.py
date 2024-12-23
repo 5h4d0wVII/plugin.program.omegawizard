2d36f1ebacd2d85421dcb568f4af53c98ac0fa96
5h4d0wz4xx <168304352+5h4d0wVII@users.noreply.github.com>
2024-12-23 23:01:57 +0000

    custom build

--- uservar.py
+++ uservar.py
@@ -13,14 +13,14 @@ ART = os.path.join(PATH, 'resources', 'media')
 #########################################################
 #        User Edit Variables                            #
 #########################################################
-ADDONTITLE = '[COLOR crimson][B]Ragnarok[/B][/COLOR]Wizard'
-BUILDERNAME = 'Ragnarok'
-EXCLUDES = [ADDON_ID, 'repository.ragnarokwizard']
+ADDONTITLE = '[COLOR steelblue][B]Omega[/B][/COLOR]Wizard'
+BUILDERNAME = 'OmegaWizard'
+EXCLUDES = [ADDON_ID, 'repository.omegawizard']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
-BUILDFILE = 'https://raw.githubusercontent.com/ragnarlothbrok-odin/Wizard/master/builds.txt'
+BUILDFILE = 'https://www.dropbox.com/scl/fi/1uzssbfausl53uuqiwuyt/builds.txt?rlkey=ikypiw719352o4lx1h0uumg8z&st=jboj2u6n&dl=1'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
-UPDATECHECK = 0
+UPDATECHECK = 4
 # Text File with apk info in it.  Leave as 'http://' to ignore
 APKFILE = 'http://'
 # Text File with Youtube Videos urls.  Leave as 'http://' to ignore
@@ -54,13 +54,13 @@ ICONLOGIN = os.path.join(ART, 'keeplogin.png')
 ICONCONTACT = os.path.join(ART, 'information.png')
 ICONSETTINGS = os.path.join(ART, 'settings.png')
 # Hide the section separators 'Yes' or 'No'
-HIDESPACERS = 'No'
+HIDESPACERS = 'Yes'
 # Character used in separator
 SPACER = '='
 
 # You can edit these however you want, just make sure that you have a %s in each of the
 # THEME's so it grabs the text from the menu item
-COLOR1 = 'crimson'
+COLOR1 = 'steelblue'
 COLOR2 = 'white'
 # Primary menu items   / {0} is the menu item and is required
 THEME1 = u'[COLOR {color1}][I]([COLOR {color1}][B]Ragnarok[/B][/COLOR][COLOR {color2}]Wizard[COLOR {color1}])[/I][/COLOR] [COLOR {color2}]{{}}[/COLOR]'.format(color1=COLOR1, color2=COLOR2)
@@ -75,9 +75,9 @@ THEME5 = u'[COLOR {color1}]Current Theme:[/COLOR] [COLOR {color2}]{{}}[/COLOR]'.
 
 # Message for Contact Page
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
-HIDECONTACT = 'No'
+HIDECONTACT = 'Yes'
 # You can add \n to do line breaks
-CONTACT = 'Thank you for choosing Ragnarok.\n\nContact us me on Discord: Ragnar Lothbrok#1948'
+CONTACT = 'Thank you for choosing OmegaWizard.\n\nContact N/A: 5h4d0wVII'
 # Images used for the contact window.  http:// for default icon and fanart
 CONTACTICON = os.path.join(ART, 'qricon.png')
 CONTACTFANART = 'http://'
@@ -96,7 +96,7 @@ AUTOUPDATE = 'Yes'
 # Enable Auto Install 'Yes' or 'No'
 AUTOINSTALL = 'No'
 # Addon ID for the repository
-REPOID = 'repository.ragnarokwizard'
+REPOID = 'repository.omegawizard'
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
 REPOADDONXML = 'https://'
 # Url to folder zip is located in
@@ -114,7 +114,7 @@ NOTIFICATION = 'http://'
 HEADERTYPE = 'Text'
 # Font size of header
 FONTHEADER = 'Font14'
-HEADERMESSAGE = '[COLOR crimson][B]Ragnarok[/B][/COLOR]Wizard'
+HEADERMESSAGE = '[COLOR steelblue][B]Omega[/B][/COLOR]Wizard'
 # url to image if using Image 424x180
 HEADERIMAGE = 'http://'
 # Font for Notification Window
